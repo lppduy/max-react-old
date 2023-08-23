a95ef13fe90d904cc73c0e451c1533122c77a8a8
lppduy <lephamphuongduy@gmail.com>
2023-08-23 15:15:20 +0700

    v47

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 7, 27);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294;
   return (
     <div className="expense-item">
-      <div>July 27th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
