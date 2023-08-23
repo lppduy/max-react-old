91999bfe4ad3680de0b3b2fd0e729cbe17bf8873
lppduy <lephamphuongduy@gmail.com>
2023-08-23 15:01:43 +0700

    v45

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <div>Title</div>
+        <div>Amout</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
