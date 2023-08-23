5f42b687f8f4ff26f56b736db89fc7fd8cc2c28c
lppduy <lephamphuongduy@gmail.com>
2023-08-23 15:07:00 +0700

    v45

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <div>Title</div>
-        <div>Amout</div>
+    <div className="expense-item">
+      <div>July 27th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
